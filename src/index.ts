5d96e8233df50637336a7643aa15a303c9d97909
BrunoAlan <alan.brunoab@gmail.com>
2024-08-24 12:52:06 -0300

    feat: Add Hero and Villain interfaces
    The code changes introduce two new interfaces, `Hero` and `Villain`, which define the structure of a hero and a villain respectively. These interfaces are added to the `interfaces` directory.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { printObject, genericFunction } from './generics/generics';
+import { Hero } from './interfaces/hero';
+import { Villain } from './interfaces/villain';
 
 // printObject(123);
 // printObject('Hello World');
@@ -6,6 +8,15 @@ import { printObject, genericFunction } from './generics/generics';
 // printObject(new Date());
 // printObject([1, 2, 3]);
 
-console.log(genericFunction(3.1416));
-console.log(genericFunction('Hello World').toUpperCase());
-console.log(genericFunction(new Date()).getDay());
+// console.log(genericFunction(3.1416));
+// console.log(genericFunction('Hello World').toUpperCase());
+// console.log(genericFunction(new Date()).getDay());
+
+const deadpool = {
+  name: 'Deadpool',
+  realName: 'Wade Wilson',
+  dangerLevel: 130,
+};
+
+console.log(genericFunction<Hero>(deadpool).name);
+console.log(genericFunction<Villain>(deadpool).dangerLevel);
