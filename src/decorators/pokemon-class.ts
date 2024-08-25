ad37b1cfb5455fcedc68537f8fb0cf1ec9126628
BrunoAlan <alan.brunoab@gmail.com>
2024-08-25 13:24:32 -0300

    refactor: Add CheckValidPokemonId decorator to Pokemon class

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -14,9 +14,30 @@ const blockPrototype = function (constructor: Function) {
   Object.seal(constructor);
   Object.seal(constructor.prototype);
 };
+
+function CheckValidPokemonId() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 800) {
+        return console.log('Invalid Pokemon ID');
+      } else {
+        return originalMethod(id);
+      }
+    };
+  };
+}
 @blockPrototype
-@printToConsoleConditional(true)
 export class Pokemon {
   public plublicApi: string = 'https://pokeapi.co';
   constructor(public name: string) {}
+
+  @CheckValidPokemonId()
+  saveToDatabase(id: number) {
+    console.log(`Saving to database with id: ${id}`);
+  }
 }
